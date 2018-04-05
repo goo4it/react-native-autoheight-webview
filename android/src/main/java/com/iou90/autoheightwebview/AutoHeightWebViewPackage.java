64918373e927ba4606251c00b41f6ec52b105a9a
goo4it <yanglinghui@ysongm.com>
2018-04-05 22:07:24 +0800

    Update AutoHeightWebViewPackage.java

--- android/src/main/java/com/iou90/autoheightwebview/AutoHeightWebViewPackage.java
+++ android/src/main/java/com/iou90/autoheightwebview/AutoHeightWebViewPackage.java
@@ -8,6 +8,7 @@ import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class AutoHeightWebViewPackage implements ReactPackage {
